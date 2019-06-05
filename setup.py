921f06524d461fb34f95d41fce7ec9b34450bc76
Nache <nache.nache@gmail.com>
2019-06-05 13:42:32 +0200

    v1.0.2 pip classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.md')).read()
 setup(
     name='django-tradukoj',
     python_requires='>=3.6.0',
-    version='1.0.1',
+    version='1.0.2',
     packages=['tradukoj'],
     description='A django IETF\'s BCP 47 DB-based translation system',
     long_description=README,
@@ -16,4 +16,22 @@ setup(
     author_email='contacto@develat.io',
     url='https://github.com/develatio/django-tradukoj/',
     license='BSD',
-    install_requires=['Django>=2.0', 'langcodes >= 1.4.1'])
+    install_requires=['Django>=2.0', 'langcodes >= 1.4.1'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Internationalization",
+        "Topic :: Software Development :: Localization",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
